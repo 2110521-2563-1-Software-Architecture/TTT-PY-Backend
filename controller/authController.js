d4a42d0894d77fb9001ee37ff464604aae3507ba
Natthanon Manop <natthanon.manop@gmail.com>
2020-10-04 23:58:10 +0700

    Fix input validation

--- controller/authController.js
+++ controller/authController.js
@@ -8,6 +8,10 @@ const { getFieldsByUsername, insertUser } = require("../model/user");
 const authController = {
   register: (req, res, next) => {
     const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return responseError(400, "Incomplete input", res);
+    }
+
     getFieldsByUsername(
       ["username", "email"],
       username,
@@ -45,50 +49,45 @@ const authController = {
     );
   },
   login: async (req, res, next) => {
-    try {
-      const { username, password } = req.body;
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return responseError(400, "Incomplete input", res);
+    }
 
-      if (!username || !password) {
-        return responseError(
-          400,
-          "Please provide an username and password",
-          res
-        );
+    if (!username || !password) {
+      return responseError(400, "Please provide an username and password", res);
+    }
+
+    getFieldsByUsername("*", username, async (err, results) => {
+      if (err) {
+        console.log(err);
+        return responseError(500, "Internal Error", res);
       }
 
-      getFieldsByUsername("*", username, async (err, results) => {
-        if (err) {
-          console.log(err);
-          return responseError(500, "Internal Error", res);
-        }
+      if (!results.length) {
+        return responseError(400, "Username or password is incorrect", res);
+      }
 
-        if (!results.length) {
-          return responseError(400, "Username or password is incorrect", res);
-        }
+      if (!(await bcrypt.compare(password, results[0].Password))) {
+        return responseError(400, "Username or password is incorrect", res);
+      }
 
-        if (!(await bcrypt.compare(password, results[0].Password))) {
-          return responseError(400, "Username or password is incorrect", res);
-        }
+      const username = results[0].Username;
 
-        const username = results[0].Username;
-
-        const token = jwt.sign({ username }, config.jwtSecret, {
-          expiresIn: config.jwtExpiresIn,
-        });
-
-        const cookieOptions = {
-          expires: new Date(
-            Date.now() + config.jwtCookieExpires * 24 * 60 * 60 * 1000
-          ),
-          httpOnly: true,
-        };
-
-        res.cookie("jwt", token, cookieOptions);
-        return responseSuccess(200, { token }, res);
+      const token = jwt.sign({ username }, config.jwtSecret, {
+        expiresIn: config.jwtExpiresIn,
       });
-    } catch (error) {
-      console.log(error);
-    }
+
+      const cookieOptions = {
+        expires: new Date(
+          Date.now() + config.jwtCookieExpires * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true,
+      };
+
+      res.cookie("jwt", token, cookieOptions);
+      return responseSuccess(200, { token }, res);
+    });
   },
 };
 
